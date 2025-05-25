694709c7e63a031dab2703a27158fd1342f83d66
Glautinor <glautinor@gmail.com>
2025-05-25 07:25:38 +0000

    rock-vendor.mk : Added libbase_shim in Product Packages

--- rock-vendor.mk
+++ rock-vendor.mk
@@ -578,6 +578,7 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/rock/proprietary/vendor/thh/ta/isee_model.json:$(TARGET_COPY_OUT_VENDOR)/thh/ta/isee_model.json
 
 PRODUCT_PACKAGES += \
+    libbase_shim \
     AVCSecureVdecCA \
     HEVCSecureVdecCA \
     VP9SecureVdecCA \
